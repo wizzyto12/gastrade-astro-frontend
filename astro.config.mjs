d84b834810bfcb4446226ac157d5375bd41dff2f
WP Infusion <info@wpinfusion.com>
2025-01-02 21:37:12 +0100

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,22 +7,30 @@ import mdx from "@astrojs/mdx";
 
 import playformInline from "@playform/inline";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "http://localhost:4321",
-	base: "/",
-	integrations: [
-		tailwind(),
-		mdx(),
-		(await import("@playform/inline")).default({
-			Critters: true,
-		}),
+  site: "http://localhost:4321",
+  base: "/",
+
+  integrations: [
+      tailwind(),
+      mdx(),
+      (await import("@playform/inline")).default({
+          Critters: true,
+      }),
 	],
-	output: "static",
-	devToolbar: {
-		enabled: false,
+
+  output: "static",
+
+  devToolbar: {
+      enabled: false,
 	},
-	experimental: {
-		svg: true,
+
+  experimental: {
+      svg: true,
 	},
+
+  adapter: netlify(),
 });
