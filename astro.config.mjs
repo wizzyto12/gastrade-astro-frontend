4ac7bbf5b25a18b09642a6232e1741aae536d581
WP Infusion <info@wpinfusion.com>
2025-02-02 01:29:53 +0100

    add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,21 @@ import mdx from "@astrojs/mdx";
 import playformInline from "@playform/inline";
 import netlify from "@astrojs/netlify";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astro-hyperdrive.netlify.app",
-	base: "/",
-	integrations: [tailwind(), mdx(), playformInline({ Critters: true })],
-	output: "static",
-	devToolbar: {
-		enabled: false,
-	},
-	experimental: {
-		svg: true,
-	},
-	adapter: netlify(),
+    site: "https://astro-hyperdrive.netlify.app",
+    base: "/",
+    integrations: [tailwind(), mdx(), playformInline({ Critters: true })],
+    output: "static",
+    devToolbar: {
+        enabled: false,
+    },
+    experimental: {
+        svg: true,
+    },
+    adapter: node({
+      mode: "standalone",
+    }),
 });
